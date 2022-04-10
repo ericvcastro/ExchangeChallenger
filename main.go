1ef0077ee0df4cf1d1c4b08aaf24d395774989c2
ericvcastro <eric.castro@coreconsulting.com.br>
2022-04-10 14:36:47 -0300

    feat: feito a rota Balance

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"exchange/ExchengeChalenger/dbconfig"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -25,8 +26,11 @@ type Wallet struct {
 }
 
 type Tokens struct {
-	Token_id int    `json:"token_id"`
-	Currency string `json:"currency"`
+	Token_id    int     `json:"token_id"`
+	Currency    string  `json:"currency"`
+	PriceDollar float64 `json:"price_dollar"`
+	PriceEuro   float64 `json:"price_euro"`
+	TimeRate    string  `json:"time_rate"`
 }
 
 type TokenWallet struct {
@@ -35,7 +39,21 @@ type TokenWallet struct {
 	Amount    float64 `json:"amount"`
 }
 
-type Balence
+type PerCurrency struct {
+	Currency               string  `json:"currency"`
+	Amount                 float64 `json:"amount"`
+	PriceInDollars         float64 `json:"price_in_dollars"`
+	PriceInEuro            float64 `json:"price_in_euro"`
+	TimeRateUsed           string  `json:"time_rate_used"`
+	TotalEurosToCurrency   float64 `json:"total_Euros_to_currency"`
+	TotalDollarsToCurrency float64 `json:"total_Dollars_to_currency"`
+}
+
+type BalanceType struct {
+	PerCurrency  []PerCurrency `json:"per_currency"`
+	TotalEuros   float64       `json:"total_Euros"`
+	TotalDollars float64       `json:"total_Dollars"`
+}
 
 func Deposit(c *gin.Context) {
 	user, okUser := c.GetQuery("user")
@@ -101,7 +119,10 @@ func Withdraw(c *gin.Context) {
 		if err != nil {
 			panic(err)
 		}
-		newAmount := amountSaved.Amount - amountFloat
+		newAmount := 0.0
+		if amountSaved.Amount > amountFloat {
+			newAmount = amountSaved.Amount - amountFloat
+		}
 		amountstring := strconv.FormatFloat(newAmount, 'E', -1, 64)
 		tokenIdString := strconv.Itoa(tokenId.Token_id)
 		walletIdString := strconv.Itoa(walletId.Wallet_id)
@@ -115,7 +136,72 @@ func Withdraw(c *gin.Context) {
 }
 
 func Balance(c *gin.Context) {
+	user, okUser := c.GetQuery("user")
+	if !okUser {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing any query parameter."})
+		return
+	}
 
+	// Recuperando o User no DB
+	userId := SelectUserToTable(user, dbconfig.TableName)
+
+	// Recuperando o Wallet ID do User no DB
+	walletId := SelectWalletToTable(userId.ID, "wallet")
+
+	tokensAlls := SelectAllTokens(walletId.Wallet_id, "tokenwallet")
+
+	balance := BalanceType{}
+	balanceTotalEuro := 0.0
+	balanceTotalDollar := 0.0
+
+	for i, s := range tokensAlls {
+		infoTokens := SelectCurrencyToTable(s.Token_id, "tokens")
+
+		euroTotal := s.Amount * infoTokens.PriceEuro
+		balanceTotalEuro += euroTotal
+
+		dollarTotal := s.Amount * infoTokens.PriceDollar
+		balanceTotalDollar += dollarTotal
+
+		perCurrency := PerCurrency{
+			Currency:               infoTokens.Currency,
+			Amount:                 s.Amount,
+			PriceInDollars:         infoTokens.PriceDollar,
+			PriceInEuro:            infoTokens.PriceEuro,
+			TimeRateUsed:           infoTokens.TimeRate,
+			TotalEurosToCurrency:   euroTotal,
+			TotalDollarsToCurrency: dollarTotal,
+		}
+		balance.PerCurrency = append(balance.PerCurrency, perCurrency)
+		fmt.Println(i, perCurrency)
+	}
+	balance.TotalDollars = balanceTotalDollar
+	balance.TotalEuros = balanceTotalEuro
+
+	c.IndentedJSON(http.StatusOK, balance)
+}
+
+func SelectAllTokens(wallet_id int, table string) []TokenWallet {
+	query := `select * from ` + table
+	row, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+
+	tokensWallet := make([]TokenWallet, 0)
+	token := TokenWallet{}
+	for row.Next() {
+		err := row.Scan(&token.Token_id, &token.Wallet_id, &token.Amount)
+		if err != nil {
+			panic(err)
+		}
+
+		if token.Wallet_id == wallet_id {
+			tokensWallet = append(tokensWallet, token)
+		}
+	}
+	return tokensWallet
 }
 
 func SelectUserToTable(user string, table string) Investor {
@@ -160,6 +246,27 @@ func SelectWalletToTable(user_Id int, table string) Wallet {
 	return wallet
 }
 
+func SelectCurrencyToTable(token_id int, table string) Tokens {
+	query := `select * from ` + table
+	row, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+
+	token := Tokens{}
+	for row.Next() {
+		err := row.Scan(&token.Token_id, &token.Currency, &token.PriceDollar, &token.PriceEuro, &token.TimeRate)
+		if err != nil {
+			panic(err)
+		}
+		if token.Token_id == token_id {
+			break
+		}
+	}
+	return token
+}
+
 func SelectTokenIdToTable(token_name string, table string) Tokens {
 	query := `select * from ` + table
 	row, err := db.Query(query)
@@ -170,7 +277,7 @@ func SelectTokenIdToTable(token_name string, table string) Tokens {
 
 	token := Tokens{}
 	for row.Next() {
-		err := row.Scan(&token.Token_id, &token.Currency)
+		err := row.Scan(&token.Token_id, &token.Currency, &token.PriceDollar, &token.PriceEuro, &token.TimeRate)
 		if err != nil {
 			panic(err)
 		}
@@ -242,5 +349,6 @@ func main() {
 	// route.GET("/investor", getAllInvestor)
 	route.PATCH("/deposit", Deposit)
 	route.PATCH("/withdraw", Withdraw)
+	route.GET("/balance", Balance)
 	route.Run("localhost:8080")
 }
