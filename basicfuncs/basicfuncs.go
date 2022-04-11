3abbb923395a84e3a9bf76e1b68c09e0b4ced5f7
ericvcastro <eric.castro023@gmail.com>
2022-04-10 22:14:27 -0300

    feat: repassando algumas funções gerais

--- basicfuncs/basicfuncs.go
+++ basicfuncs/basicfuncs.go
@@ -1,11 +1,30 @@
 package basicfuncs
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
-func createValuesTableWallet(db *sql.DB, table string, token_id int, user_id int, amount float64) {
+func CreateValuesTableWallet(db *sql.DB, table string, token_id int, user_id int, amount float64) {
 	addValuesToTable := `INSERT INTO ` + table + ` VALUES ($1, $2, $3)`
 	_, err := db.Exec(addValuesToTable, token_id, token_id, amount)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func AddValuesTable(db *sql.DB, table string, user_id int, transaction string, time_realized string) {
+	addValuesToTable := `INSERT INTO ` + table + ` VALUES ($1, $2, $3)`
+	_, err := db.Exec(addValuesToTable, user_id, transaction, time_realized)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func UpdateDB(db *sql.DB, table string, setCamp string, dataToUpdate string, whereModify string) {
+	query := `update ` + table + ` SET ` + setCamp + ` = ` + dataToUpdate + ` WHERE ` + whereModify
+	row, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
 }
